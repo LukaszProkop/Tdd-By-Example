bbde6414ed6a0f975b5033755558bf2e562548dc
LukaszProkop <wuzu222@gmail.com>
2020-11-30 05:35:31 +0100

    refactor code (cont.)

--- src/main/java/com/tdd/by/example/Money.java
+++ src/main/java/com/tdd/by/example/Money.java
@@ -2,7 +2,7 @@ package com.tdd.by.example;
 
 import java.util.Objects;
 
-public abstract class Money {
+public class Money {
 
     protected int amount;
     protected String currency;
@@ -16,8 +16,6 @@ public abstract class Money {
         return currency;
     }
 
-    public abstract Money times(int multiplier);
-
     public static Money dollar(int amount) {
         return new Dollar(amount, "USD");
     }
@@ -26,16 +24,29 @@ public abstract class Money {
         return new Franc(amount, "CHF");
     }
 
+    public Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null) return false;
         Money money = (Money) o;
-        return amount == money.amount;
+        return amount == money.amount
+                && this.currency == money.currency;
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(amount);
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
     }
 }
