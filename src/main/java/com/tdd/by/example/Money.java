58b48000a3c0880b4cace1c7f3372089e791c4cd
LukaszProkop <wuzu222@gmail.com>
2020-11-30 04:49:26 +0100

    add inheritance to reduce duplicates (cont.)

--- src/main/java/com/tdd/by/example/Money.java
+++ src/main/java/com/tdd/by/example/Money.java
@@ -2,9 +2,18 @@ package com.tdd.by.example;
 
 import java.util.Objects;
 
-abstract class Money {
+public abstract class Money {
 
     protected int amount;
+
+    public abstract Money times(int multiplier);
+
+    public static Money dollar(int amount){
+        return new Dollar(amount);
+    }
+    public static Money franc(int amount){
+        return new Franc(amount);
+    }
 
     @Override
     public boolean equals(Object o) {
