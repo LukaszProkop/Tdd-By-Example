10169ec13b7a355b46c2493b8a09cddfdc166990
LukaszProkop <wuzu222@gmail.com>
2020-12-01 01:37:14 +0100

    fix

--- src/main/java/com/tdd/by/example/Expression.java
+++ src/main/java/com/tdd/by/example/Expression.java
@@ -1,6 +1,8 @@
 package com.tdd.by.example;
 
 public interface Expression {
+    Expression times(int multiplier);
+
     Expression plus(Expression added);
 
     Money reduce(Bank bank, String to);
